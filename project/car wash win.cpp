caf517ac6a7084a788ccb0b8f60c6e09c9d98a38
Madkita <abubakaryagob@gmail.com>
2018-05-11 02:14:38 +0800

    added windows specific version

--- project/car wash win.cpp
+++ project/car wash win.cpp
@@ -1,16 +1,19 @@
+// for windows only!!
 /* group members:
 	- Abubakar Yagoub Ibrahim     1625897
  	- Hanan Allil 			 		   1617888
 	- Ichsanudin Fukky Winasis     G1710308
  */
-// time to start commenting everything for no reason(because why not)
-// too many sleep() to make it more realistic
-// between both if-else and switch for diversity
+// time to start commenting everything for no reason(because why not).
+// some parts have no comments because they're self explanatory.
+// too many Sleep() to make it more realistic.
+// using both if-else and switch for diversity.
+// all input and operations on variables are done with pointers(because why not!!).
 #include <iostream> // standard library
 #include <iomanip> // output manipulation library
 #include <cstring> // C library (for strcpy)
-#include "payment.h" // external header file
-#include <ctime> //time library
+#include "payment win.h" // external header file
+#include <ctime> // time library
 using namespace std;
 char name[] = "AHK"; // combination of team names initials.
 void tags(); // decoration, separating between different parts of the program
@@ -21,17 +24,17 @@ void Another_Service(); // function to let user choose another service after fin
 int main()
 {
 	cout << "			--------------------------------------------------\n\n";
-	cout << "				Welcome To " << name <<  " Car Washing Service\n" << endl; // need a name for this
+	cout << "				Welcome To " << name <<  " Car Washing Service\n" << endl;
 	cout << "			--------------------------------------------------\n\n";
-	sleep(2);
+	Sleep(2);
 	cout << "\nPlease Select Vehicle Type: " << endl << endl // prompt user to select vehicle type.
 	     << "1. Car" << endl
 	     << "2. Motor Bike" << endl
 	     << "3. Truck " << endl <<  endl;
-	cin >> *ptr_veh;// should be vehicle type (check)
+	cin >> *ptr_veh;
 	while (*ptr_veh > 3 || *ptr_veh < 1) // just in case you think you're smart.
 	{
-		sleep(1);
+		Sleep(1);
 		cout << "Please Select A Valid Option: ";
 		cin >> *ptr_veh;
 	}
@@ -43,7 +46,7 @@ int main()
 	strcpy(Serv[3], "Steam");
 	strcpy(Serv[4], "Vacuum");
 	strcpy(Serv[5], "Wax");
-	sleep(2); // stop the program for 2 seconds.
+	Sleep(2); // stop the program for 2 seconds.
 	cout << "\nSelect Service: " << endl << endl;
 	for (int i = 1; i <= 6; ++i) // output array content by cell
 	{
@@ -51,9 +54,9 @@ int main()
 	}
 	cout << endl;
 	cin >> *ptr_serv;
-	while (*ptr_serv > 6 || *ptr_serv < 1) // just in case you think you're smart
+	while (*ptr_serv > 6 || *ptr_serv < 1) // just in case you think you're smart.
 	{
-		sleep(1);
+		Sleep(1);
 		cout << "Please Select A Valid Option: ";
 		cin >> *ptr_serv;
 	}
@@ -64,10 +67,10 @@ int main()
 void Services() // services function.
 {
 	tags();
-	sleep(2);
+	Sleep(2);
 	if (*ptr_serv == 1)
 	{
-		/* the main difference between the 3 types is tools used to perform the operation
+		/* the main difference between the 3 types is tools used to perform the operation and the amount of attention to details
 		premium type includes all things from Advanced and Basic with some extras as well */
 		cout << "\nSelect The Type Of Wash You Want: \n" << endl
 		     << setw(20) << left << "1. Basic Wash" << setw(3) << left << "RM" << w_price[0] << endl
@@ -76,27 +79,27 @@ void Services() // services function.
 		cin >> *ptr_w;
 		while (*ptr_w > 3 || *ptr_w < 1) // you're not smart
 		{
-			sleep(1);
+			Sleep(1);
 			cout << "Please Select A Valid Option: ";
 			cin >> *ptr_w;
 		}
-		sleep(1);
+		Sleep(1);
 		switch (*ptr_w) // give visual output of selected service before processing.
 		{
 		case 1: cout << "\nBasic Wash Selected\n" << endl;
-			sleep(1);
+			Sleep(1);
 			cout << "Proceeding To Payment..." << endl << endl; break;
 		case 2: cout << "\nAdvanced Wash Selected\n" << endl;
-			sleep(1);
+			Sleep(1);
 			cout << "Proceeding To Payment..." << endl << endl; break;
 		case 3: cout << "\nPremium Wash Selected\n" << endl;
-			sleep(1);
+			Sleep(1);
 			cout << "Proceeding To Payment..." << endl << endl; break;
 		}
 	}
 	if (*ptr_serv == 2)
 	{
-		/* the main difference between the 3 types is tools used to perform the operation
+		/* the main difference between the 3 types is tools used to perform the operation and the amount of attention to details
 		premium type includes all things from Advanced and Basic with some extras as well */
 		cout << "\nPlease Select The  Scrub Type You Want: \n" << endl
 		     << setw(20) << left << "1. Basic Scrub" << setw(3) << left << "RM" << scrub_price[0] << endl
@@ -105,46 +108,46 @@ void Services() // services function.
 		cin >> *ptr_scrub;
 		while (*ptr_scrub > 3 || *ptr_scrub < 1) // you should be aware of that by now
 		{
-			sleep(1);
+			Sleep(1);
 			cout << "Please Select A Valid Option: ";
 			cin >> *ptr_scrub;
 		}
-		sleep(1);
+		Sleep(1);
 		switch (*ptr_scrub) // give visual output of selected service before processing.
 		{
 		case 1: cout << "\nBasic Scrub Selected\n" << endl;
-			sleep(1);
+			Sleep(1);
 			cout << "Proceeding To Payment..." << endl << endl; break;
 		case 2: cout << "\nAdvanced Scrub Selected\n" << endl;
-			sleep(1);
+			Sleep(1);
 			cout << "Proceeding To Payment..." << endl << endl; break;
 		case 3: cout << "\nPremium Scrub Selected\n" << endl;
-			sleep(1);
+			Sleep(1);
 			cout << "Proceeding To Payment..." << endl << endl; break;
 		}
 	}
 	if (*ptr_serv == 3)
 	{
 		cout << "\nRinse Service Selected\n" << endl; // give visual output of selected service before processing.
-		sleep(2);
+		Sleep(2);
 		cout << "Proceeding To Payment..." << endl << endl;
 	}
 	if (*ptr_serv == 4)
 	{
 		cout << "\nSteam Service Selected...\n" << endl; // give visual output of selected service before processing.
-		sleep(2);
+		Sleep(2);
 		cout << "Proceeding To Payment..." << endl << endl;
 	}
 	if (*ptr_serv == 5)
 	{
 		cout << "\nVacuum Service Selected...\n" << endl; // give visual output of selected service before processing.
-		sleep(2);
+		Sleep(2);
 		cout << "Proceeding To Payment..." << endl << endl;
 	}
 	if (*ptr_serv == 6)
 	{
 		cout << "\nWax Service Selected\n" << endl; // give visual output of selected service before processing.
-		sleep(2);
+		Sleep(2);
 		cout << "Proceeding To Payment..." << endl << endl;
 	}
 	tags();
@@ -156,25 +159,25 @@ void Services() // services function.
 void Receipt()
 {
 	tags();
-	sleep(1);
+	Sleep(1);
 	cout << endl << "Starting Your Service..." << endl;
-	sleep(2);
+	Sleep(2);
 	cout << "\npress Any Key To Start: ";
 	cin >> start_yn; // input anything here just to make sure the user wants the service to start now
-	sleep(2);
+	Sleep(2);
 	cout << endl << "Service ongoing..." << endl;
-	sleep(10); // assuming in these 10 seconds  the operation is completed
+	Sleep(10); // assuming in these 10 seconds  the operation is completed
 	cout << "\nOperation Completed!!" << endl << endl; // skip 2 lines
-	sleep(2);
+	Sleep(2);
 	tags();
 	cout << endl <<  "Here Is Your Receipt:" << endl << endl;
-	sleep(2);
+	Sleep(2);
 	tags();
 	Meta(); //call function to generate service id, time, and date
 	cout <<  endl << setw(15) << left <<  "Date " << ": " << week[s + 1] << ", " << mon_char[mon - 1] << " " << day + 1 << "th" << " " << year << endl;
-	sleep(1);
+	Sleep(1);
 	cout << endl << setw(15) << left <<  "Service ID" << ": " << serv_id << endl;
-	sleep(1);
+	Sleep(1);
 	cout << endl << setw(15) << left <<  "Vehicle Type" << ": ";
 	switch (*ptr_veh)
 	{
@@ -182,7 +185,7 @@ void Receipt()
 	case 2 : cout << "Motor Bike"; break;
 	case 3 : cout << "truck"; break;
 	}
-	sleep(1);
+	Sleep(1);
 	cout << endl <<  endl <<  setw(15) << left <<  "Service Type" << ": ";
 	switch (*ptr_serv)
 	{
@@ -217,8 +220,8 @@ void Receipt()
 	case 5 : cout << "Vacuum" << endl; break;
 	case 6 : cout << "Wax" << endl; break;
 	}
-	sleep(1);
-	cout << endl <<  setw(15) << left <<  "Payment Amount" << ": RM" << total << endl << endl; // total is the payment amount, no algorithm needed
+	Sleep(1);
+	cout << endl <<  setw(15) << left <<  "Payment Amount" << ": RM" << total << endl << endl;
 	tags();
 	cout << endl << "Would You Like To Perform Another Operation (yes or no)? ";
 	cin >> as_yn;
@@ -226,12 +229,12 @@ void Receipt()
 	// for some reason strcmp can't compare pointers so used the variable itself
 	while (strcmp(as_yn, "yes") != 0 && strcmp(as_yn, "no") != 0 && strcmp(as_yn, "Yes") != 0 && strcmp(as_yn, "No") != 0) // again testing your IQ level.
 	{
-		sleep(1);
+		Sleep(1);
 		cout << endl <<  "Please Type Yes Or No: ";
 		cin >> as_yn;
 	}
-	sleep(2);
-	// new option to check if user wants to perform another operation or not
+	Sleep(2);
+	// check if user wants to perform another service.
 	if (strcmp(as_yn, "yes" ) == 0 || strcmp(as_yn, "Yes") == 0) // let both capital and small initials be true
 	{
 		Another_Service(); // function call.
@@ -239,10 +242,10 @@ void Receipt()
 	}
 	else if (strcmp(as_yn, "no") == 0 || strcmp(as_yn, "No") == 0) // let both capital and small initials be true
 	{
-		sleep(1);
+		Sleep(1);
 		tags();
 		cout << "\nthank you for using our services" << endl;
-		sleep(1);
+		Sleep(1);
 		cout << "\nPlease come back again!!" << endl << endl; // or maybe not.
 
 	}
@@ -258,7 +261,7 @@ void Meta() // good stuff right here.
 	strcpy(week[4], "Thu") ;
 	strcpy(week[5], "Fri") ;
 	strcpy(week[6], "Sat") ;
-	srand(time(NULL)); // set run time to null so you get a different number each time you run the program.
+	srand(time(NULL)); // set run time to null so you get a different number each time you run the function.
 	serv_id = 1 + rand() % (1 - 1000 + 1); // generate a number between 1 and 1000.
 	time_t t = time(NULL); // set the time to null so it will read the right system data everytime you run the program.
 	struct tm tm = *localtime(&t); // a structure to get the local date
@@ -289,7 +292,7 @@ void Meta() // good stuff right here.
 
 void Another_Service() // allow user To select another operation.
 {
-	// wont ask for vehicle type again because its known now.
+	// wont ask for vehicle type again because its already  known by now.
 	int i = 1; // for do-while
 	tags();
 	// get the other operation user wants to perform and start all over again
@@ -299,11 +302,11 @@ void Another_Service() // allow user To select another operation.
 		cout << i << ". " << Serv[i - 1] << endl;
 		++i;
 	} while (i <=  6);
-	cout << endl;
+	cout << endl; // formatting 101.
 	cin >> *ptr_serv;
 	while (*ptr_serv > 6 || *ptr_serv < 1) // just confirming YOU'RE NOT SMART!!
 	{
-		sleep(1);
+		Sleep(1);
 		cout << "Please Select A Valid Option: ";
 		cin >> *ptr_serv;
 	}
---
+++ project/payment win.h
@@ -0,0 +1,216 @@
+// for windows only!!
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <unistd.h>
+using namespace std;
+// global declarations in a header file and yes it does work.
+const int  rinse_price = 10, steam_price = 15, vacuum_price = 10, wax_price = 5;
+const int veichle_price[3] = {20, 15, 40}; // array for prices car, motor, truck.
+const int w_price[3] = {15, 20, 30}; // Wash prizes based on type
+const int scrub_price[3] = {10, 20, 40}; // scrub prizes based on type
+int vehicle_type, service, price, w_type, scrub_type, total, day, mon, year, serv_id, day_nxt, year_nxt, mon_nxt, s;
+char start_yn[100], Serv[6][20], as_yn[5], week[7][10], mon_char[12][20] ; // array to output services
+int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // number of days each month has.
+int *ptr_veh = &vehicle_type, *ptr_serv = &service, *ptr_w = &w_type, *ptr_scrub = &scrub_type; // using pointers for some values.
+// realized this thing is gonna be huge(but simple) so moved to a separate header file.
+void Payment()
+{
+	Sleep(1);
+
+	if (*ptr_serv == 1)
+	{
+
+		if (*ptr_w == 1)
+		{
+			switch (*ptr_veh)
+			{
+			case 1 :
+				total = w_price[0] + veichle_price[0]; // using total variable instead of live output
+				cout << "The Total Price For Your Basic Car Wash Is: RM" << total << endl;// array should start from index [0]
+				break;
+			case 2 :
+				total = w_price[0] + veichle_price[1];
+				cout << "The Total Price For Your Basic Bike Wash Is: RM" << total << endl;
+				break;
+			case 3 :
+				total = w_price[0] + veichle_price[2];
+				cout << "The Total Price For Your Basic Truck Wash Is: RM" << total << endl;
+				break;
+			}
+		}
+		if (*ptr_w == 2)
+		{
+			switch (*ptr_veh)
+			{
+
+			case 1 :
+				total = w_price[1] + veichle_price[0];
+				cout << "The Total Price For Your Advanced Car Wash Is: RM" << total << endl;
+				break;
+			case 2 :
+				total = w_price[1] + veichle_price[1];
+				cout << "The Total Price For Your Advanced Bike Wash Is: RM" << total << endl;
+				break;
+			case 3 :
+				total = w_price[1] + veichle_price[2];
+				cout << "The Total Price For Your Advanced Truck Wash Is: RM" << total << endl;
+				break;
+			}
+		}
+		if (*ptr_w == 3)
+		{
+			switch (*ptr_veh)
+			{
+			case 1 :
+				total = w_price[2] + veichle_price[0];
+				cout << "The Total Price For Your Premium Car Wash Is: RM" << total << endl;
+				break;
+			case 2 :
+				total = w_price[2] + veichle_price[1];
+				cout << "The Total Price For Your Premium Bike Wash Is: RM" << total << endl;
+				break;
+			case 3 :
+				total = w_price[2] + veichle_price[2];
+				cout << "The Total Price For Your Premium Truck Wash Is: RM" << total << endl;
+				break;
+			}
+		}
+	}
+	if (*ptr_serv == 2)
+	{
+
+		if (*ptr_scrub == 1)
+		{
+			switch (*ptr_veh)
+			{
+			case 1 :
+				total = scrub_price[0] + veichle_price[0];
+				cout << "The Total Price For Your Basic Car Scrub Is: RM" << total << endl;
+				break;
+			case 2 :
+				total = scrub_price[0] + veichle_price[1];
+				cout << "The Total Price For Your Basic Bike Scrub Is: RM" << total << endl;
+				break;
+			case 3 :
+				total = scrub_price[0] + veichle_price[2];
+				cout << "The Total Price For Your Basic Truck Scrub Is: RM" << total << endl;
+				break;
+			}
+		}
+		if (*ptr_scrub == 2)
+		{
+			switch (*ptr_veh)
+			{
+			case 1 :
+				total = scrub_price[1] + veichle_price[0];
+				cout << "The Total Price For Your Advanced Car Scrub Is: RM" << total << endl;
+				break;
+			case 2 :
+				total = scrub_price[1] + veichle_price[1];
+				cout << "The Total Price For Your Advanced Bike Scrub Is: RM" << total << endl;
+				break;
+			case 3 :
+				total = scrub_price[1] + veichle_price[2];
+				cout << "The Total Price For Your Advanced Truck Scrub Is: RM" << total << endl;
+				break;
+			}
+		}
+		if (*ptr_scrub == 3)
+		{
+			switch (*ptr_veh)
+			{
+			case 1 :
+				total = scrub_price[2] + veichle_price[0];
+				cout << "The Total Price For Your Premium Car scrub Is: RM" << total << endl;
+				break;
+			case 2 :
+				total = scrub_price[2] + veichle_price[1];
+				cout << "The Total Price For Your Premium Bike scrub Is: RM" << total << endl;
+				break;
+			case 3 :
+				total = scrub_price[2] + veichle_price[2];
+				cout << "The Total Price For Your Premium Truck scrub Is: RM" << total << endl;
+				break;
+			}
+		}
+	}
+	if (*ptr_serv == 3)
+	{
+		if (*ptr_veh == 1)
+		{
+			total = rinse_price + veichle_price[0];
+			cout << "The Total Price For Your  Car Rinse Is: RM" << total << endl;
+
+		}
+		else if (*ptr_veh == 2)
+		{
+			total = rinse_price + veichle_price[1];
+			cout << "The Total Price For Your  Bike Rinse Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 3)
+		{
+			total = rinse_price + veichle_price[2];
+			cout << "The Total Price For Your  Truck Rinse Is: RM" << total << endl;
+		}
+	}
+	if (*ptr_serv == 4)
+	{
+		if (*ptr_veh == 1)
+		{
+			total = steam_price + veichle_price[0];
+			cout << "The Total Price For Your  Car Steam Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 2)
+		{
+			total = steam_price + veichle_price[1];
+			cout << "The Total Price For Your  Bike Steam Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 3)
+		{
+			total = steam_price + veichle_price[2];
+			cout << "The Total Price For Your  Truck Steam Is: RM" << total << endl;
+		}
+	}
+	if (*ptr_serv == 5)
+	{
+		if (*ptr_veh == 1)
+		{
+			total = vacuum_price + veichle_price[0];
+			cout << "The Total Price For Your  Car Vacuum Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 2)
+		{
+			total = vacuum_price + veichle_price[1];
+			cout << "The Total Price For Your  Bike Vacuum Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 3)
+		{
+			total = vacuum_price + veichle_price[2];
+			cout << "The Total Price For Your  Truck Vacuum Is: RM" << total << endl;
+			total = vacuum_price + veichle_price[2];
+		}
+	}
+	if (*ptr_serv == 6)
+	{
+		if (*ptr_veh == 1)
+		{
+			total = wax_price + veichle_price[0];
+			cout << "The Total Price For Your  Car Wax Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 2)
+		{
+			total = wax_price + veichle_price[1];
+			cout << "The Total Price For Your  Bike Wax Is: RM" << total << endl;
+		}
+		else if (*ptr_veh == 3)
+		{
+			total = wax_price + veichle_price[2];
+			cout << "The Total Price For Your  Truck Wax Is: RM" << total << endl;
+		}
+	}
+	Sleep(2);
+	cout << "\n" << "Please Pay at The Counter..." << "\n\n";
+	Sleep(5); // will assume that he made the payment in those 5 seconds
+	cout << "Payment Received!" << endl << endl;
+}
